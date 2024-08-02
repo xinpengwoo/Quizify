a44869e809bf88c9f0d947f3c8afdfbe6e88d9c4
xinpengwoo <xinpengwoo@gmail.com>
2024-08-02 16:57:09 +0800

    quizify

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     
     embed_config = {
         "model_name": "textembedding-gecko@003",
-        "project": "quizzify-430709",
+        "project": "YOUR-PROJECT-ID-HERE",
         "location": "us-central1"
     }
     screen = st.empty()
